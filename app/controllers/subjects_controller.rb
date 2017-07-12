7970d8fbe12f51cedf4c4d73d3cbf46b76ed1831
Administrator <Administrator@RASHEED.LOCAL>
2017-07-12 13:36:55 +0300

    insert and form

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,15 +1,26 @@
 class SubjectsController < ApplicationController
   def index
+    @subjects = Subject.sorted
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def edit
   end
 
+  def new
+    @subject = Subject.new
+  end
+
   def create
-    
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to subjects_path
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -17,10 +28,15 @@ class SubjectsController < ApplicationController
   end
 
   def delete
-    
+
   end
 
   def destroy
     
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit(:name,:position,:visible)
   end
 end
